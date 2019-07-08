2e213be53993d6273d693d3a4d7fda798a34072d
arunya15 <https://github.com/arunya15>
2019-07-08 12:37:28 +0530

    fun

--- mainz.js
+++ mainz.js
@@ -1,46 +1,430 @@
-
-
 SC.initialize({
-//  client_id: '340f063c670272fac27cfa67bffcafc4'
-client_id: '340f063c670272fac27cfa67bffcafc4'
+
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+
 });
 
-//$(document).ready(function() {   SC.stream("/tracks/181294675",function(sound){
+$(document).ready(function() {
 
-  $(document).ready(function() {
-     SC.stream("/tracks/212104562",function(sound){
+  SC.stream('/tracks/210606753',function(sound){
 
-  $('#start1').click(function(e) {
-           e.preventDefault();
-           sound.start();
-         });
 
-$('#stop1').click(function(e) {
-           e.preventDefault();
-           sound.stop();
-         });
+
+       $("#start").click(function(e) {
+
+              e.preventDefault();
+
+              sound.start();
+
+             });
+
+
+
+
+
+              $("#stop").click(function(e) {
+
+                 e.preventDefault();
+
+                 sound.stop();
+
+               });
+
 });
-  SC.stream("/tracks/475197861",function(sound){
-  $('#start2').click(function(e) {
-           e.preventDefault();
-           sound.start();
-         });
 
-$('#stop2').click(function(e) {
-           e.preventDefault();
-           sound.stop();
-         });
+
+
+//ngk peranppea
+
+
+
+SC.stream('/tracks/621668781',function(sound){
+
+
+
+     $("#start1").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop1").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
 });
 
-   SC.stream("/tracks/237436326",function(sound){
-  $('#start3').click(function(e) {
-           e.preventDefault();
-           sound.start();
-         });
 
-$('#stop3').click(function(e) {
-           e.preventDefault();
-           sound.stop();
-         });
+
+
+
+//thupaki google
+
+
+
+
+
+SC.stream('/tracks/63007631',function(sound){
+
+
+
+     $("#start2").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop2").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
 });
+
+
+
+//maari2 mydear macha
+
+
+
+SC.stream('/tracks/556842441',function(sound){
+
+
+
+     $("#start3").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop3").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+
+
+//naanum rowdy thaan
+
+
+
+SC.stream('/tracks/317968951',function(sound){
+
+
+
+     $("#start4").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop4").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+//veeram nallavanu sollvaanka
+
+
+
+SC.stream('/tracks/125437193',function(sound){
+
+
+
+     $("#start5").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop5").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+
+
+
+
+//darmadurai aandipatti kanavaa
+
+
+
+SC.stream('/tracks/284005521',function(sound){
+
+
+
+     $("#start6").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop6").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+
+
+//iravukuAayiram naan mulusaa
+
+
+
+SC.stream('/tracks/397422978',function(sound){
+
+
+
+     $("#start7").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop7").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+
+
+
+
+//theeran sevatha pulla
+
+
+
+SC.stream('/tracks/349414693',function(sound){
+
+
+
+     $("#start8").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop8").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+
+
+
+
+// sethupathi konchipesida
+
+
+
+SC.stream('/tracks/303954855',function(sound){
+
+
+
+     $("#start9").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop9").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+
+
+
+
+//ispdae rajavum kannammaa unnai
+
+
+
+SC.stream('/tracks/602054046',function(sound){
+
+
+
+     $("#start10").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop10").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
+
+
+
+
+
+//natpeathunai singlepasanka
+
+
+
+SC.stream('/tracks/584885949',function(sound){
+
+
+
+     $("#start11").click(function(e) {
+
+            e.preventDefault();
+
+            sound.start();
+
+           });
+
+
+
+
+
+            $("#stop11").click(function(e) {
+
+               e.preventDefault();
+
+               sound.stop();
+
+             });
+
+});
+
+
 });
